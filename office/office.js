7cec612b1686fbb2bc715fe0fcdd2836774017d6
Erik Uzureau <erik.uzureau@datadoghq.com>
2019-10-07 11:39:47 +0200

    Initial Commit

---
+++ office/office.js
@@ -0,0 +1,22 @@
+// Format an office object for easy display
+function formatOffice(office) {
+    const displayOffice = {};
+    displayOffice.name = office.name;
+
+    const street = office.address.street;
+    const city = office.address.city;
+    const country = office.address.country;
+    const zip = office.address.zip;
+
+    // Simple address foomat
+    displayOffice.address = `${street}, ${city}, ${country} ${zip}`;
+
+    displayOffice.drivers =
+        office.drivers
+            .map(driver => `${driver.first} ${driver.last}`)
+            .join(', ');
+
+    return displayOffice;
+};
+
+module.exports.formatOffice = formatOffice;
