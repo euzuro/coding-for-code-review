66daeba5e031c4d76abd9192b728d38c1500b8e1
Erik Uzureau <erik.uzureau@datadoghq.com>
2019-10-07 12:21:29 +0200

    Add office/ package and rename tests so they will not run

---
+++ office/office.js
@@ -0,0 +1,20 @@
+// Format an office objet for easy display
+function formatOffice(office) {
+    const displayOffice = {};
+    displayOffice.name = office.name;
+
+    const street = office.address.street;
+    const city = office.address.city;
+    const country = office.address.country;
+    const zip = office.address.zip;
+    displayOffice.address = `${street}, ${city}, ${country} ${zip}`;
+
+    displayOffice.drivers =
+        office.drivers
+            .map(driver => `${driver.first} ${driver.last}`)
+            .join(', ');
+
+    return displayOffice;
+};
+
+module.exports.formatOffice = formatOffice;
