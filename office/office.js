ff5109d1b907b57c9c95a5a4a1973b69a038278d
Erik Uzureau <erik.uzureau@datadoghq.com>
2019-10-08 17:50:27 +0200

    :sparkles: Order by seniority and show seniority

--- office/office.js
+++ office/office.js
@@ -1,12 +1,16 @@
 // Format a driver for display
 function formatDriver(driver) {
-    return `${driver.first} ${driver.last}`;
+    return `${driver.first} ${driver.last} (${driver.seniority})`;
 }
 
+function driverComparator(d1, d2) {
+    return d2.seniority - d1.seniority;
+}
 
 // Format drivers for display
 function formatDrivers(drivers) {
     return drivers
+        .sort(driverComparator)
         .map(formatDriver)
         .join(', ');
 }
