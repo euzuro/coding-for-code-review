33bb3b40b360f5a44f9b48a976f384ad569b6a6a
Erik Uzureau <erik.uzureau@datadoghq.com>
2019-10-09 07:54:35 +0200

    :recycle: Decompose driver formatting

--- office/office.js
+++ office/office.js
@@ -1,3 +1,15 @@
+// Format driver for easy display
+function formatDriver(driver) {
+    return `${driver.first} ${driver.last}`;
+}
+
+// Format drivers for easy display
+function formatDrivers(drivers) {
+    return drivers
+        .map(formatDriver)
+        .join(', ');
+}
+
 // Format an office object for easy display
 function formatOffice(office) {
     const displayOffice = {};
@@ -13,10 +25,8 @@ function formatOffice(office) {
     const zip = address.zip;
     displayOffice.address = `${street}, ${city}, ${country} ${zip}`;
 
-    displayOffice.drivers =
-        office.drivers
-            .map(driver => `${driver.first} ${driver.last}`)
-            .join(', ');
+    // Simple drivers format
+    displayOffice.drivers = formatDrivers(office.drivers);
 
     return displayOffice;
 };
