c1b328ee1372f5830fe29eaa31235f46146dab11
Erik Uzureau <erik.uzureau@datadoghq.com>
2019-11-15 01:22:10 +0100

    :sparkles: Show seniority of drivers:

--- office/office.js
+++ office/office.js
@@ -1,6 +1,6 @@
 // Format driver for easy display
 function formatDriver(driver) {
-    return `${driver.first} ${driver.last}`;
+    return `${driver.first} ${driver.last} (${driver.seniority})`;
 }
 
 // Format drivers for easy display
