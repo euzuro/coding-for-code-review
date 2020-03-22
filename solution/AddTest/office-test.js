0b59457cba22915ff73b345352bd42ef069edda1
Erik Uzureau <erik.uzureau@datadoghq.com>
2020-03-22 17:43:00 +0100

    :alien:[Office] wrap address and drivers in 'meta'

--- solution/AddTest/office-test.js
+++ solution/AddTest/office-test.js
@@ -7,24 +7,26 @@ describe('office', function () {
             const OFFICE = {
                 id: 1423,
                 name: "Bordeaux",
-                address: {
-                    street: "39 Rue du Château d'Eau",
-                    city: "Bordeaux", country: "France", zip: "33000",
-                },
-                drivers: [{
-                    id: 3, vehicle: 'renault',
-                    first: 'Daniel', last: 'RICCARDO',
-                    seniority: 5, age: 30,
-                }, {
-                    id: 33, vehicle: 'Honda',
-                    first: 'Max', last: 'VERSTAPPEN',
-                    seniority: 3, age: 22,
-                }, {
-                    id: 14, vehicle: 'mercedes',
-                    first: 'Lewis', last: 'HAMILTON',
-                    seniority: 12, age: 34,
-                },
-                ]
+                meta: {
+                    address: {
+                        street: "39 Rue du Château d'Eau",
+                        city: "Bordeaux", country: "France", zip: "33000",
+                    },
+                    drivers: [{
+                        id: 3, vehicle: 'renault',
+                        first: 'Daniel', last: 'RICCARDO',
+                        seniority: 5, age: 30,
+                    }, {
+                        id: 33, vehicle: 'Honda',
+                        first: 'Max', last: 'VERSTAPPEN',
+                        seniority: 3, age: 22,
+                    }, {
+                        id: 14, vehicle: 'mercedes',
+                        first: 'Lewis', last: 'HAMILTON',
+                        seniority: 12, age: 34,
+                    },
+                    ]
+                }
             };
             const EXPECTED = {
                 name: "Bordeaux",
